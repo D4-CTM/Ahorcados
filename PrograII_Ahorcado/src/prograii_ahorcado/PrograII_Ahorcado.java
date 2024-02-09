4f7877833c5a4a04233f2bb63cb1c696db7d8e15
Josh <140992634+D4-CTM@users.noreply.github.com>
2024-02-09 13:09:10 -0600

    First_Commit

---
+++ PrograII_Ahorcado/src/prograii_ahorcado/PrograII_Ahorcado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prograii_ahorcado;
+
+/**
+ *
+ * @author josue
+ */
+public class PrograII_Ahorcado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
